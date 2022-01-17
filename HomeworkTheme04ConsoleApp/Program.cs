2a441adcff6671f71bb6d87c55260c286d9e543c
katzhukova <katzhukova@gmail.com>
2022-01-17 15:50:32 +0300

    Реализовала генерацию случайного целого числа из диапазона (от 0 до «введено пользователем»)

--- HomeworkTheme04ConsoleApp/Program.cs
+++ HomeworkTheme04ConsoleApp/Program.cs
@@ -10,58 +10,61 @@ namespace HomeworkTheme04ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Задание 1. Случайная матрица");
-            Console.Write("Введите количество строк в будущей матрице: ");
-            int row = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Задание 1. Случайная матрица");
+            //Console.Write("Введите количество строк в будущей матрице: ");
+            //int row = int.Parse(Console.ReadLine());
 
-            Console.Write("Введите количество столбцов в будущей матрице: ");
-            int col = int.Parse(Console.ReadLine());
+            //Console.Write("Введите количество столбцов в будущей матрице: ");
+            //int col = int.Parse(Console.ReadLine());
 
-            int[,] matrix = new int[row, col];
+            //int[,] matrix = new int[row, col];
 
-            int sum = 0;
+            //int sum = 0;
 
-            Random random = new Random();
+            //Random random = new Random();
 
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    matrix[i, j] = random.Next(0, 10);
-                    Console.Write($" {matrix[i, j]}\t");
-                    sum += matrix[i, j];
-                }
-                Console.WriteLine();
-            }
+            //for (int i = 0; i < row; i++)
+            //{
+            //    for (int j = 0; j < col; j++)
+            //    {
+            //        matrix[i, j] = random.Next(0, 10);
+            //        Console.Write($" {matrix[i, j]}\t");
+            //        sum += matrix[i, j];
+            //    }
+            //    Console.WriteLine();
+            //}
 
-            Console.WriteLine($"Cуммa всех элементов этой матрицы: {sum} ");
+            //Console.WriteLine($"Cуммa всех элементов этой матрицы: {sum} ");
 
-            Console.WriteLine("Задание 2. Наименьший элемент в последовательности");
-            Console.Write("Введите длину последовательности: ");
-            int lengthArray = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Задание 2. Наименьший элемент в последовательности");
+            //Console.Write("Введите длину последовательности: ");
+            //int lengthArray = int.Parse(Console.ReadLine());
 
-            int[] userArray = new int[lengthArray];
+            //int[] userArray = new int[lengthArray];
 
-            Console.WriteLine($"Введите {lengthArray} элементов массива");
-            for (int i = 0; i < lengthArray; i++)
-            {
-                userArray[i] = int.Parse(Console.ReadLine());
-            }
+            //Console.WriteLine($"Введите {lengthArray} элементов массива");
+            //for (int i = 0; i < lengthArray; i++)
+            //{
+            //    userArray[i] = int.Parse(Console.ReadLine());
+            //}
 
-            int minValueUserArray = int.MaxValue;
-            for (int i = 0; i < lengthArray; i++)
-            {
-                if (userArray[i]<minValueUserArray)
-                {
-                    minValueUserArray = userArray[i];
-                }
-            }
+            //int minValueUserArray = int.MaxValue;
+            //for (int i = 0; i < lengthArray; i++)
+            //{
+            //    if (userArray[i]<minValueUserArray)
+            //    {
+            //        minValueUserArray = userArray[i];
+            //    }
+            //}
 
-            Console.WriteLine($"Наименьший элемент в данном массиве {minValueUserArray}");
+            //Console.WriteLine($"Наименьший элемент в данном массиве {minValueUserArray}");
 
             Console.WriteLine("Задание 3. Игра \"Угадай число\"");
             Console.Write("Введите верхнюю границу диапазона: ");
             int userNumber = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, userNumber);
 
             Console.ReadKey();
         }
